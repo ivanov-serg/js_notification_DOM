8062236cc62b2c3f32a9cc17b2656c2476e17ecd
Sergey Ivanov <ivanovsereza26@gmail.com>
2026-04-22 19:56:46 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,38 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
+  const notification = document.createElement('div');
+
+  notification.classList.add('notification', type);
+
+  // позиція
+  notification.style.position = 'fixed';
+  notification.style.top = `${posTop}px`;
+  notification.style.right = `${posRight}px`;
+
+  // заголовок
+  const titleEl = document.createElement('h2');
+
+  titleEl.classList.add('title');
+  titleEl.textContent = title;
+
+  // опис
+  const descEl = document.createElement('p');
+
+  descEl.textContent = description;
+
+  // додаємо елементи в контейнер
+  notification.append(titleEl, descEl);
+
+  // додаємо в DOM
+  document.body.appendChild(notification);
+
+  // через 2 секунди ховаємо (НЕ видаляємо!)
+  setTimeout(() => {
+    notification.style.display = 'none';
+    // або:
+    // notification.style.visibility = 'hidden';
+  }, 2000);
   // write code here
 };
 
